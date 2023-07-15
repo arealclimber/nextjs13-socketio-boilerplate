1e20a365b04bb572c1b60dff0fc3761ee6ac8655
arealclimber <luffulgence@gmail.com>
2023-07-16 03:10:17 +0800

    feat: 🎸 show others' msg on chat

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 import {
-    Button,
-    Card,
-    Container,
-    Group,
-    Text,
-    Divider,
-    Input,
-    ScrollArea,
-    TextInput,
-    ActionIcon,
+	Button,
+	Card,
+	Container,
+	Group,
+	Text,
+	Divider,
+	Input,
+	ScrollArea,
+	TextInput,
+	ActionIcon,
 } from "@mantine/core";
 import { IconArrowRight } from "@tabler/icons-react";
 import NameModal from "@/components/NameModal";
@@ -19,86 +19,88 @@ import { useEffect, useRef, useState } from "react";
 import useBasicStore from "@/store/basic";
 
 export default function Home() {
-    const storeName = useBasicStore((state) => state.name);
-    const [socket, emit] = useSocketStore((state) => [state.socket, state.emit]);
+	const storeName = useBasicStore((state) => state.name);
+	const [socket, emit] = useSocketStore((state) => [state.socket, state.emit]);
 
-    const inputRef = useRef<HTMLInputElement>(null);
-    const [name, setName] = useState<string | null>(null); // avoiding Next.js hydration error
-    const [message, setMessage] = useState("");
-    const [messages, setMessages] = useState<string[]>([]);
+	const inputRef = useRef<HTMLInputElement>(null);
+	const [name, setName] = useState<string | null>(null); // avoiding Next.js hydration error
+	const [message, setMessage] = useState("");
+	const [messages, setMessages] = useState<string[]>([]);
 
-    const [opened, { open, close }] = useDisclosure(false);
+	const [opened, { open, close }] = useDisclosure(false);
 
-    const sendMessage = () => {
-        emit("message", message);
-        setMessages([...messages, message]);
-        setMessage("");
-        inputRef.current?.focus();
-    };
+	const sendMessage = () => {
+		emit("message", message);
+		// setMessages((prevMessages) => [...prevMessages, message]);
+		setMessage("");
+		inputRef.current?.focus();
+	};
 
-    useEffect(() => {
-        setName(storeName);
-        if (!storeName) open();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [storeName]);
+	useEffect(() => {
+		setName(storeName);
+		if (!storeName) open();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [storeName]);
 
-    useEffect(() => {
-        socket?.on("message", (data) => {
-            console.log("message", data);
-        });
-        return () => {
-            socket?.off("message");
-        };
-    }, [socket]);
+	useEffect(() => {
+		socket?.on("message", (data) => {
+			console.log("message", data);
 
-    return (
-        <>
-            <Container pt="sm" size="md">
-                <Card shadow="sm" padding="sm" radius="md" withBorder>
-                    <Group position="apart" mt="xs" mb="xs">
-                        <Group>
-                            <Text size="xl" weight={500} suppressHydrationWarning={true}>
-                                {name}
-                            </Text>
-                            <Button size="xs" radius="md" onClick={open} variant="light">
-                                Change name
-                            </Button>
-                        </Group>
-                        <Input placeholder="Your target Socket ID" />
-                    </Group>
+			setMessages((prevMessages) => [...prevMessages, data]);
+		});
+		return () => {
+			socket?.off("message");
+		};
+	}, [socket]);
 
-                    <Divider />
-                    <ScrollArea h={"80vh"} offsetScrollbars>
-                        {messages.map((message, index) => (
-                            <Text key={index}>{message}</Text>
-                        ))}
-                    </ScrollArea>
-                    <Divider />
+	return (
+		<>
+			<Container pt="sm" size="md">
+				<Card shadow="sm" padding="sm" radius="md" withBorder>
+					<Group position="apart" mt="xs" mb="xs">
+						<Group>
+							<Text size="xl" weight={500} suppressHydrationWarning={true}>
+								{name}
+							</Text>
+							<Button size="xs" radius="md" onClick={open} variant="light">
+								Change name
+							</Button>
+						</Group>
+						<Input placeholder="Your target Socket ID" />
+					</Group>
 
-                    <TextInput
-                        ref={inputRef}
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        mt={10}
-                        radius="xl"
-                        size="md"
-                        rightSection={
-                            <ActionIcon size={32} radius="xl" variant="filled">
-                                <IconArrowRight size="1.1rem" stroke={1.5} onClick={sendMessage} />
-                            </ActionIcon>
-                        }
-                        placeholder="Type something..."
-                        rightSectionWidth={42}
-                        onKeyDown={(e) => {
-                            if (e.key === "Enter") {
-                                sendMessage();
-                            }
-                        }}
-                    />
-                </Card>
-            </Container>
+					<Divider />
+					<ScrollArea h={"80vh"} offsetScrollbars>
+						{messages.map((message, index) => (
+							<Text key={index}>{message}</Text>
+						))}
+					</ScrollArea>
+					<Divider />
 
-            <NameModal opened={opened} onClose={close} />
-        </>
-    );
+					<TextInput
+						ref={inputRef}
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+						mt={10}
+						radius="xl"
+						size="md"
+						rightSection={
+							<ActionIcon size={32} radius="xl" variant="filled">
+								<IconArrowRight size="1.1rem" stroke={1.5} onClick={sendMessage} />
+							</ActionIcon>
+						}
+						placeholder="Type something..."
+						rightSectionWidth={42}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								sendMessage();
+							}
+						}}
+					/>
+				</Card>
+			</Container>
+
+			<NameModal opened={opened} onClose={close} />
+		</>
+	);
 }
